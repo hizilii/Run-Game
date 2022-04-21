ffbcaab2a3c94b97ebb4368524dcf0f30ed1e65c
hizilii <104042427+hizilii@users.noreply.github.com>
2022-04-22 00:25:51 +0900

    Update Unity_ChanController.cs

--- Assets/Scripts/Unity_ChanController.cs
+++ Assets/Scripts/Unity_ChanController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Unity_ChanController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    
     void Start()
     {
-        
+        animator=GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 前進
+        if(Input.GetKey("up")){
+            transform.position+=transform.forward*0.01f;
+            animator.SetBool("run",true);
+        }else{
+            animator.SetBool("run",false);
+        }
+
+        // 左右回転
+        if(Input.GetKey("left")){
+            transform.Rotate(0,-1,0);
+        }else if(Input.GetKey("right")){
+            transform.Rotate(0,1,0);
+        }
+
+        // ジャンプ
+        if(Input.GetKey("space")){
+            animator.SetBool("jump",true);
+        }else{
+            animator.SetBool("jump",false);
+        }
     }
 }
